47914072b0c761e2d06acdb0e40069a4b79eead8
Romulo Moraes <limamoraes_7@hotmail.com>
2025-09-29 19:21:47 -0400

    adding the new function sub

--- src/script.js
+++ src/script.js
@@ -1,17 +1,30 @@
-function add(a,b) {
+function add(a, b) {
     return a + b;
 }
 
-function calculate(a, b) {
+function sub(a, b) {
+    return a - b;
+}
+
+function calculate(option) {
     const num1 = Number(document.getElementById('num1').value);
     const num2 = Number(document.getElementById('num2').value);
+    let result;
 
-    const result = add(num1, num2);
-
+    switch (option) {
+        case "add": 
+            result = add(num1, num2);
+            break;
+        case "sub":
+            result = sub(num1, num2);
+            break;
+    }
+  
     document.getElementById("result").innerText = "Result:" + result;
 }
 
+//exporting to jest
 if (typeof module !== 'undefined'){
-    module.exports = { add };
+    module.exports = {add, sub};
 }
 
--- test/script.test.js
+++ test/script.test.js
@@ -1,5 +1,11 @@
 const { add } = require("../src/script");
 
+const { sub } = require("../src/script");
+
 test("add 2 + 3 = 5", () => {
   expect(add(2, 3)).toBe(5);
 });
+
+test("sub 5 - 3 = 2", () => {
+  expect(sub(5, 3)).toBe(2);
+})
